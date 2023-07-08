8d39d94e3cb511ac811c8349aaac03daec851199
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-07-08 13:00:03 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ const client_id = process.env.CLIENT_ID;
 const type = process.env.TYPE;
 const project_id = process.env.PROJECT_ID;
 const private_key_id = process.env.PRIVATE_KEY_ID;
-const private_key = process.env.PRIVATE_KEY;
+const private_key = JSON.parse(process.env.PRIVATE_KEY);
 const client_email = process.env.CLIENT_EMAIL;
 const auth_uri = process.env.AUTH_URI;
 const token_uri = process.env.TOKEN_URI;
@@ -28,7 +28,7 @@ const serviceAccount = {
   type: type,
   project_id: project_id,
   private_key_id: private_key_id,
-  private_key: private_key,
+  private_key,
   client_email: client_email,
   client_id: client_id,
   auth_uri: auth_uri,
