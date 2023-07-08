3ae88db647c82425287c17b08bd2cb24fb954297
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2023-07-08 19:34:34 +0530

    removed dotenv

--- Backend/index.js
+++ Backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 var randomstring = require("randomstring");
-require("dotenv").config();
+// require("dotenv").config();
 const {
   initializeApp,
   applicationDefault,
